af9af7e74677de2f43d571ff0553c2c077e26e41
tb-aiap <towboon@gmail.com>
2025-08-13 22:19:23 +0800

    feat: added parser function to read from sysargv.

--- src/daily_minesweeper/utils.py
+++ src/daily_minesweeper/utils.py
@@ -27,3 +27,30 @@ def get_sample_minesweeper_game(url_address: str = None) -> str:
     driver.quit()
 
     return str(result)
+
+
+def parse_sysargv_difficulty(args: list[str]) -> str:
+    """Get the difficulty from command line.
+
+    Expects from a list of difficulty in constants.py, defaults to daily if none entered.
+
+    Args:
+        args (list[str]): from sys.argv
+
+    Raises:
+        Exception: if difficulty is not in expected list
+
+    Returns:
+        str: one of the constants predefined.
+    """
+    if len(args) == 1:
+        return c.DAILY
+
+    difficulty = args[1]
+    if difficulty not in c.DIFFICULTY:
+        raise Exception(
+            f"expecting one of the following options {c.DIFFICULTY}",
+            f"received {difficulty}",
+        )
+
+    return getattr(c, difficulty.upper())
